ba7d90ab0e01c5db45eaf2208518517bb1d24420
Seamus Lowry <spl1owry3@gmail.com>
2022-12-02 06:23:41 -0500

    2022/2 (#3)
    * first puzzle
    
    * second puzzle
    
    * second puzzle

---
+++ 2022/day2/puzzle1/main.py
@@ -0,0 +1,22 @@
+
+ROUNDS_TO_RESULTS = {
+  "A X": 1 + 3,
+  "A Y": 2 + 6,
+  "A Z": 3 + 0,
+  "B X": 1 + 0,
+  "B Y": 2 + 3,
+  "B Z": 3 + 6,
+  "C X": 1 + 6,
+  "C Y": 2 + 0,
+  "C Z": 3 + 3
+}
+
+input = open("input.txt", "r")
+
+score = 0
+
+for line in input:
+  score += ROUNDS_TO_RESULTS[line[:3]]
+
+
+print(score)

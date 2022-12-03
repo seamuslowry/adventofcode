6e29aa5aeede422ed2be8f864886684f62cd8fa6
Seamus Lowry <spl1owry3@gmail.com>
2022-12-03 08:29:31 -0500

    part one

--- 2022/day3/main.py
+++ 2022/day3/main.py
@@ -1,10 +1,22 @@
 
+priority = "?abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+def get_priority(char: str) -> int:
+  return priority.index(char)
+
+def find_match(rucksack: str) -> str:
+  middle = int(len(rucksack)/2)
+  left = rucksack[:middle]
+  right = rucksack[middle:]
+  overlap = [l for l in left if l in right]
+  return overlap[0]
+
 def p1():
-  return 0
+  input = open("input.txt", "r")
+  return sum(get_priority(find_match(l)) for l in input)
 
 def p2():
   return 0
-
 
 print(p1())
 print(p2())

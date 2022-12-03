588693b0d060f6d802df3c0108a3f8bad7f91271
Seamus Lowry <spl1owry3@gmail.com>
2022-12-03 08:36:27 -0500

    part two

--- 2022/day3/main.py
+++ 2022/day3/main.py
@@ -11,12 +11,21 @@ def find_match(rucksack: str) -> str:
   overlap = [l for l in left if l in right]
   return overlap[0]
 
+def find_badge(rucksacks: list[str]) -> str:
+  overlap = [l for l in rucksacks[0] if l in rucksacks[1] and l in rucksacks[2]]
+  return overlap[0]
+
 def p1():
   input = open("input.txt", "r")
   return sum(get_priority(find_match(l)) for l in input)
 
 def p2():
-  return 0
+  input = open("input.txt", "r").read()
+  sacks = input.split("\n")
+  badges = []
+  for index in range(0, len(sacks), 3):
+    badges.append((find_badge(l[index:index+3])) for l in input)
+  return sum(badges)
 
 print(p1())
 print(p2())

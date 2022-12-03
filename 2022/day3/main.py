a46139109c0a2bbe9f3578da69036378cbc77589
Seamus Lowry <spl1owry3@gmail.com>
2022-12-03 08:40:26 -0500

    2022/3 (#6)
    * add base folder
    
    * move base and copy to day 3
    
    * part one
    
    * part two
    
    * part two fixes

---
+++ 2022/day3/main.py
@@ -0,0 +1,31 @@
+
+priority = "?abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+def get_priority(char: str) -> int:
+  return priority.index(char)
+
+def find_match(rucksack: str) -> str:
+  middle = int(len(rucksack)/2)
+  left = rucksack[:middle]
+  right = rucksack[middle:]
+  overlap = [l for l in left if l in right]
+  return overlap[0]
+
+def find_badge(rucksacks: list[str]) -> str:
+  overlap = [l for l in rucksacks[0] if l in rucksacks[1] and l in rucksacks[2]]
+  return overlap[0]
+
+def p1():
+  input = open("input.txt", "r")
+  return sum(get_priority(find_match(l)) for l in input)
+
+def p2():
+  input = open("input.txt", "r").read()
+  sacks = input.split("\n")
+  badges = []
+  for index in range(0, len(sacks), 3):
+    badges.append(get_priority(find_badge(sacks[index:index+3])))
+  return sum(badges)
+
+print(p1())
+print(p2())

fff63cd5bda763edb8cd784529ef006360bc4742
Seamus Lowry <spl1owry3@gmail.com>
2022-12-06 18:05:21 -0500

    p1

--- 2022/day06/main.py
+++ 2022/day06/main.py
@@ -1,7 +1,13 @@
 
+def find_first_distinct_of_length(s: str, l: int) -> int:
+  for index in range(l, len(s)):
+    if len(set([*s[index-l:index]])) == l:
+      return index
+  return -1
+
 def p1():
-  input = open("input.txt", "r")
-  return 0
+  input = open("input.txt", "r").read()
+  return find_first_distinct_of_length(input, 4)
 
 def p2():
   input = open("input.txt", "r")

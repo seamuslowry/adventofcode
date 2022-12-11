486166f12fec1c03efd3561c92f1b77071f1b279
Seamus Lowry <spl1owry3@gmail.com>
2022-12-11 12:49:33 -0500

    part 2

--- 2022/day11/main.py
+++ 2022/day11/main.py
@@ -1,20 +1,19 @@
-
 from dataclasses import dataclass
 from functools import reduce
-from typing import Callable
-from pprint import pprint
 
 @dataclass
 class Monkey:
   items: list[int]
-  operation: str # requires variables 'new' and 'old' in scope
-  test: Callable[[int], int] # given items worry level, return the index of the monkey to throw to
+  operation: str # requires variable 'old' in scope
+  divisor: int
+  true_index: int
+  false_index: int
 
-  def inspect(self, old: int) -> int: # return the new worry level of the item
-    return eval(self.operation) // 3
+  def inspect(self, old: int, worry_modifier: int) -> int: # return the new worry level of the item
+    return eval(self.operation) // worry_modifier
 
   def decide_throw(self, item: int) -> int: # return the index to throw to
-    return self.test(item)
+    return self.true_index if (item % self.divisor == 0) else self.false_index
 
 
 def process_monkey_definition(paragraph: str) -> Monkey:
@@ -25,24 +24,26 @@ def process_monkey_definition(paragraph: str) -> Monkey:
   true_index = int(lines[4].split(" ")[-1])
   false_index = int(lines[5].split(" ")[-1])
 
-  return Monkey(items, operation, lambda worry: true_index if (worry % divisor == 0) else false_index)
+  return Monkey(items, operation, divisor, true_index, false_index)
 
-def act(all_monkeys: list[Monkey], monkey: Monkey) -> int: # return number of items inspected
-  new_worries = list(map(monkey.inspect, monkey.items))
+def act(all_monkeys: list[Monkey], monkey: Monkey, worry_modifier: int) -> int: # return number of items inspected
+  new_worries = list(map(lambda item: monkey.inspect(item, worry_modifier), monkey.items))
+  all_divisor = reduce(lambda a,b: a*b, map(lambda m: m.divisor, all_monkeys))
   throws = list(map(monkey.decide_throw, new_worries))
   for index, throw in enumerate(throws):
-    all_monkeys[throw].items.append(new_worries[index])
+    dest_monkey = all_monkeys[throw]
+    dest_monkey.items.append(new_worries[index] % all_divisor)
   monkey.items.clear()
 
   return len(throws)
 
-def round(all_monkeys: list[Monkey]) -> dict[int, int]:
-  return {index: act(all_monkeys, monkey) for index, monkey in enumerate(all_monkeys)}
+def round(all_monkeys: list[Monkey], worry_modifier: int) -> dict[int, int]:
+  return {index: act(all_monkeys, monkey, worry_modifier) for index, monkey in enumerate(all_monkeys)}
 
-def process(all_monkeys: list[Monkey]) -> dict[int,int]:
+def process(all_monkeys: list[Monkey], rounds: int, worry_modifier: int) -> dict[int,int]:
   ret = {}
-  for _ in range(0,20):
-    round_results = round(all_monkeys)
+  for _ in range(0, rounds):
+    round_results = round(all_monkeys, worry_modifier)
     for key, value in round_results.items():
       ret[key] = ret.get(key, 0) + value
   return ret
@@ -54,13 +55,15 @@ def determine_monkey_business(aggregation: dict[int,int]) -> int:
 def p1():
   input = open("input.txt", "r").read().split("\n\n")
   monkeys = list(map(process_monkey_definition, input))
-  aggregation = process(monkeys)
+  aggregation = process(monkeys, 20, 3)
   return determine_monkey_business(aggregation)
 
 def p2():
-  input = open("input.txt", "r")
-  return 0
+  input = open("input.txt", "r").read().split("\n\n")
+  monkeys = list(map(process_monkey_definition, input))
+  aggregation = process(monkeys, 10000, 1)
+  return determine_monkey_business(aggregation)
 
 
-pprint(p1())
+print(p1())
 print(p2())

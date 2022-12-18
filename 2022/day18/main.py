c0590c2871faae892b1b6eacda08739d0212ced3
Seamus Lowry <spl1owry3@gmail.com>
2022-12-18 09:57:07 -0500

    part one

--- 2022/day18/main.py
+++ 2022/day18/main.py
@@ -1,7 +1,43 @@
 
+from dataclasses import dataclass, astuple
+
+
+@dataclass(eq=True, frozen=True)
+class Point:
+  x: int
+  y: int
+  z: int
+
+  def __iter__(self):
+    return iter(astuple(self))
+
+
+def parse_input(lines: list[str]) -> set[Point]:
+  vals: set[Point] = set()
+  for line in lines:
+    x,y,z = line.split(",")
+    vals.add(Point(int(x),int(y),int(z)))
+  return vals
+
+def uncovered_sides(points: set[Point], point: Point):
+  covered = 0
+  for mod in [-1,1]:
+    covered += int(Point(point.x + mod, point.y, point.z) in points)
+    covered += int(Point(point.x, point.y + mod, point.z) in points)
+    covered += int(Point(point.x, point.y, point.z + mod) in points)
+
+  return 6 - covered
+
+def total_uncovered_sides(points: set[Point]):
+  uncovered = 0
+  for point in points:
+    uncovered += uncovered_sides(points, point)
+
+  return uncovered
+
 def p1():
-  input = open("input.txt", "r")
-  return 0
+  input = open("input.txt", "r").read().splitlines()
+  return total_uncovered_sides(parse_input(input))
 
 def p2():
   input = open("input.txt", "r")

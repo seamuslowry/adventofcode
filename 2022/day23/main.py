ea2b1e4ba2f9934bc4d2900bb791d69c061d2f00
Seamus Lowry <spl1owry3@gmail.com>
2022-12-23 20:17:22 -0500

    starting out

--- 2022/day23/main.py
+++ 2022/day23/main.py
@@ -1,12 +1,40 @@
+from pprint import pprint
+from dataclasses import dataclass
+from enum import Enum
+
+class Direction(Enum):
+  NORTH = 0
+  SOUTH = 1
+  WEST = 2
+  EAST = 3
+
+@dataclass(frozen=True, eq=True)
+class Position:
+  row: int
+  col: int
+
+def play_round(round_number: int, starting_positions: set[Position]) -> set[Position]:
+  proposal_direction = Direction(round_number % len(Direction))
+
+  return set()
+
+def parse_input(map: str) -> set[Position]:
+  ret = set()
+  for row, line in enumerate(map.splitlines()):
+    for col, char in enumerate(line):
+      if char == '#':
+        ret.add(Position(row=row, col=col))
+  return ret
+
 
 def p1():
-  input = open("input.txt", "r")
-  return 0
+  input = open("input.txt", "r").read()
+  return parse_input(input)
 
 def p2():
   input = open("input.txt", "r")
   return 0
 
 
-print(p1())
+pprint(p1())
 print(p2())

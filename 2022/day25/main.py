0968d34218033fef7e22929b860ce7a9f07c18f9
Seamus Lowry <spl1owry3@gmail.com>
2022-12-25 13:39:01 -0500

    2022/25 (#30)
    * part one
    
    * no base anymore
    
    * no part 2 for last day
    
    * dont need current index

--- 2022/day25/main.py
+++ 2022/day25/main.py
@@ -1,12 +1,44 @@
 
-def p1():
-  input = open("input.txt", "r")
-  return 0
+UNADJUSTED_SNAFU = {
+  '=': -2,
+  '-': -1,
+  '0': 0,
+  '1': 1,
+  '2': 2
+}
 
-def p2():
-  input = open("input.txt", "r")
-  return 0
+ADJUSTED_SNAFU = {
+  0: '0',
+  1: '1',
+  2: '2',
+  3: '=',
+  4: '-'
+}
+
+def snafu_to_decimal(num: str) -> int:
+  ret = 0
+  for index, char in enumerate(reversed(num)):
+    ret += UNADJUSTED_SNAFU[char] * 5**index
+
+  return ret
+
+def decimal_to_snafu(original_num: int) -> str:
+  if original_num == 0:
+    return '0'
+
+  current_num = original_num
+  ret = ""
+
+  while current_num > 0:
+    ret += ADJUSTED_SNAFU[current_num % 5]
+    current_num = (current_num + 2) // 5
+
+  return ret[::-1]
+
+def p1():
+  input = open("input.txt", "r").read().splitlines()
+  dec_sum = sum(snafu_to_decimal(l) for l in input)
+  return decimal_to_snafu(dec_sum)
 
 
 print(p1())
-print(p2())

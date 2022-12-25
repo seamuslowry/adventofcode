71cdb2523ef9f57ef5a6162b33e0683080110e25
Seamus Lowry <spl1owry3@gmail.com>
2022-12-25 13:21:37 -0500

    dont need current index

--- 2022/day25/main.py
+++ 2022/day25/main.py
@@ -28,11 +28,9 @@ def decimal_to_snafu(original_num: int) -> str:
 
   current_num = original_num
   ret = ""
-  current_index = 0
 
   while current_num > 0:
     ret += ADJUSTED_SNAFU[current_num % 5]
-    current_index += 1
     current_num = (current_num + 2) // 5
 
   return ret[::-1]
